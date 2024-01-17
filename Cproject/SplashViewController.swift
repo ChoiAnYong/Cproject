97933593d1e7c979d1acd688ce2afcdb9479aa1b
ChoiAnYong <dksdyd78@naver.com>
2024-01-17 20:56:23 +0900

    스플래시 화면 구현

---
+++ Cproject/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  Cproject
+//
+//  Created by 최안용 on 1/17/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+}
