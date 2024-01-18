6cc8b3bae280ee4296240f734c68821ee0fcc1a9
ChoiAnYong <dksdyd78@naver.com>
2024-01-18 16:54:30 +0900

    Lottie 이용하여 애니메이션 구현

--- Cproject/SplashViewController.swift
+++ Cproject/SplashViewController.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
-    @IBOutlet weak var appIconCenterXConstraint: NSLayoutConstraint!
-    @IBOutlet weak var appIconCenterYConstraint: NSLayoutConstraint!
-    @IBOutlet weak var appIcon: UIImageView!
+    @IBOutlet weak var lottieAnimationView: LottieAnimationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,18 +19,6 @@ class SplashViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        //좌측 상단으로 이동하는 애니메이션
-//        appIconCenterXConstraint.constant = -(view.frame.width / 2) - (appIcon.frame.width / 2)
-//        appIconCenterYConstraint.constant = -(view.frame.height / 2) - (appIcon.frame.height / 2)
-//        
-//        UIView.animate(withDuration: 1) { [weak self] in
-//            self?.view.layoutIfNeeded()
-//        }
-        
-        //회전하는 애니메이션
-        UIView.animate(withDuration: 3) { [weak self] in
-            let rotationAngle: CGFloat = CGFloat.pi
-            self?.appIcon.transform = CGAffineTransform(rotationAngle: rotationAngle)
-        }
+        lottieAnimationView.play()
     }
 }
